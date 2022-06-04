4cd709a06b85ae6589b1214edba515c810bdac5e
Debdeep <6978309+debdeepbh@users.noreply.github.com>
2022-06-04 02:00:14 -0500

    readymade linear regression via Sequential neural network

--- torch/linreg_ready.py
+++ torch/linreg_ready.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+from torch import nn
 from torch.utils import data
 
 # source: https://d2l.ai/chapter_linear-networks/linear-regression-concise.html
@@ -26,10 +27,44 @@ features, labels = synthetic_data(true_w, true_b, 1000)
 #######################################################################
 # create batches
 
-def load_array(data_arrays, batch_size, is_train=True):  
+def load_array(data_arrays, batch_size, shuffle=True):  
     """Construct a PyTorch data iterator."""
     dataset = data.TensorDataset(*data_arrays)
-    return data.DataLoader(dataset, batch_size, shuffle=is_train)
+    return data.DataLoader(dataset, batch_size, shuffle=shuffle)
 
 batch_size = 10
 data_iter = load_array((features, labels), batch_size)
+
+## print the first batch
+# print(next(iter(data_iter)))
+
+# model: single layer Sequential neural network
+# model: 2 = x-dim; 1 = y-dim
+net = nn.Sequential(nn.Linear(2,1))
+
+# initialize the weights and the bias of the first layer
+net[0].weight.data.normal_(0, 0.01)
+net[0].bias.data.fill_(0)
+
+# loss function; default: reduction='mean'; 
+loss = nn.MSELoss()
+
+# training algorithm: SGD; parameters to optimize: net.parameters(); learning rate = lr
+trainer = torch.optim.SGD(net.parameters(), lr=0.03)
+
+
+# training process 
+num_epochs = 3
+for epoch in range(num_epochs):
+    for X, y in data_iter:
+        # l obtained through nn.MSELoss() already has the sum (then avg) of the loss over the batch
+        l = loss(net(X) ,y)
+        # print('l=',l)
+        # trainer.zero_grad() grad is same as setting zero_grad() to each parameter in net.parameters()
+        trainer.zero_grad()
+        l.backward()
+        # step() will update the value of the minimizer parameters
+        trainer.step()
+    # again, nn.MSELoss() computes the mean of all the errors
+    l = loss(net(features), labels)
+    print(f'epoch {epoch + 1}, loss {l:f}')
